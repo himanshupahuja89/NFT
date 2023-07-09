be13a12d7599c29742aad7ab15d171e4953c5a72
himanshupahuja89 <himansh10b@gmail.com>
2023-07-09 15:28:11 +0530

    added Card Component

--- Components/Card/Card.jsx
+++ Components/Card/Card.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Style from "./Card.module.css";
+import images from "../Image/client/index";
+import imagesNFT from "../Image/index";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ setNotification, image, index }) => {
+  return (
+    <div class={Style.card}>
+      <div class={Style.content}>
+        {/* <a href={`/image/${image.imageId}`}> */}
+        <a href={`/image/1`}>
+          <p>
+            {/* <img */}
+            <Image
+              className={Style.image}
+              // src={image.image}
+              src={imagesNFT.img1}
+              alt="image"
+              width={250}
+              height="200"
+            />
+          </p>
+        </a>
+        <span class={Style.para}>
+          <Image
+            className="avatar_img"
+            // src={images[`client${index + 1}`]}
+            src={images[`client1`]}
+            width={40}
+            height={40}
+          />
+          <small
+            className={Style.para_small}
+            onClick={() => (
+              setNotification("Successfully copied"),
+              // navigator.clipboard.writeText(image.owner)
+              navigator.clipboard.writeText("Successfully copied")
+            )}
+          >
+            0x23534564634534645634634
+            {/* {image.owner.slice(0, 25)}... */}
+          </small>
+        </span>
+        <span>
+          {/* CreatedAt: {new Date(image.createdAt * 1000).toDateString()} */}
+          JUL 10 2023
+          {/* <small className={Style.number}>#{image.imageID}</small> */}
+          <small>#1</small>
+        </span>
+        {/* <small class={Style.para}>{image.description.slice(0, 80)}..</small> */}
+        <small class={Style.para}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi odit
+          aliquam ..
+        </small>
+
+        <button
+          onClick={() => (
+            setNotification("Image URL is successfully copied"),
+            // navigator.clipboard.writeText(image.image)
+            navigator.clipboard.writeText("Image URL is successfully copied")
+          )}
+          class={Style.btn}
+        >
+          Copy URL
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- pages/layout.js
+++ pages/layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Logo, Button } from '../Components'
+import { Logo, Button, Card } from '../Components'
 
 const layout = () => {
   return (
     <div className='home'><Logo />
-    <Button/>
+      <Button />
+      <Card />
     </div>
   )
 }
